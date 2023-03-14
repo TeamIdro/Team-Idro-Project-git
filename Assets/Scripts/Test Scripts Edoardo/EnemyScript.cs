250f6d713376a6e760078913555675e5421d82b2
Edoardo Del Sarto <e99delsarto@gmail.com>
2023-03-14 18:04:09 +0100

    - Varius enemy Update

--- Assets/Scripts/Test Scripts Edoardo/EnemyScript.cs
+++ Assets/Scripts/Test Scripts Edoardo/EnemyScript.cs
@@ -5,7 +5,7 @@ using UnityEngine.AI;
 using BehaviorDesigner.Runtime;
 using System;
 
-public class EnemyTestScript : MonoBehaviour, IEnemy
+public class EnemyScript : MonoBehaviour, IEnemy
 {
     [field: SerializeField] public EnemyCategory category { get; set; }
     [field: SerializeField] public Weakness weakness { get; set; }
@@ -16,6 +16,8 @@ public class EnemyTestScript : MonoBehaviour, IEnemy
     [field: SerializeField] public SpriteRenderer spriteRenderer { get; set; }
     
     [field: SerializeField] public float angle { get; set; }
+    [field: SerializeField] public float fovAngle { get; set; }
+    [field: SerializeField] public float viewDistance { get; set; }
     [field: SerializeField] public bool canPatrol { get; set; }
 
     private NavMeshAgent agent;
@@ -32,6 +34,8 @@ public class EnemyTestScript : MonoBehaviour, IEnemy
 		agent.updateRotation = false;
 		agent.updateUpAxis = false;
         // agent.stoppingDistance = behaviorTree.GetVariable()
+
+        
     }
 
     public void Start()
@@ -41,11 +45,18 @@ public class EnemyTestScript : MonoBehaviour, IEnemy
         spriteRenderer = GetComponent<SpriteRenderer>();
 
         angle = 280f;
+        fovAngle = 120f;
+        viewDistance = 7.4f;
         // canPatrol = true;
     }
 
     public void Update()
     {
+        if(hp <= 0)
+        {
+            Destroy(this.gameObject);
+        }
+
         transform.position = new Vector3(transform.position.x, transform.position.y, 0f);
     }
 
@@ -62,5 +73,25 @@ public class EnemyTestScript : MonoBehaviour, IEnemy
             attackCooldown -= Time.deltaTime;
         }
         
+    }
+
+    public void GetDamage()
+    {
+        Debug.Log("COLPITO");
+        hp -= 100;
+    }
+
+    public void ReduceSeeRange()
+    {
+        angle = 280f;
+        fovAngle = 120f;
+        viewDistance = 7.4f;
+    }
+
+    public void IncreaseSeeRange()
+    {
+        angle = 280f;
+        fovAngle = 360f;
+        viewDistance = 10f;
     }
 }
