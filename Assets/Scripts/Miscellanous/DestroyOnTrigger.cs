a5e2b16552f1db976d387e5bfcccca1cb84ca27b
RedViper234 <84193022+RedViper234@users.noreply.github.com>
2023-03-28 19:30:14 +0200

    Aggiunta estensione LayerMask 2.0

--- Assets/Scripts/Miscellanous/DestroyOnTrigger.cs
+++ Assets/Scripts/Miscellanous/DestroyOnTrigger.cs
@@ -2,14 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public static class LayerMaskExtensions
-{
-    public static bool IsInLayerMask(this GameObject obj, LayerMask mask)
-    {
-        return ((mask.value & (1 << obj.layer)) > 0);
-    }
-}
-
 public class DestroyOnTrigger : MonoBehaviour
 {
     public LayerMask ignoreContact;
