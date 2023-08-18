20755d1410e05263e2e21412387511cce519e486
Edoardo Del Sarto <e99delsarto@gmail.com>
2023-08-18 11:48:12 +0200

    - Modifiche navmesh - salto da piattaforma migliorato

--- Assets/Scripts/Player/DetectMovingPlatform.cs
+++ Assets/Scripts/Player/DetectMovingPlatform.cs
@@ -1,6 +1,5 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+
 
 
 public class DetectMovingPlatform : MonoBehaviour
@@ -10,32 +9,64 @@ public class DetectMovingPlatform : MonoBehaviour
     public LayerMask terrainMask;
     RaycastHit2D hit;
     public float offsetXCast;
+    public float jumpForceBoost;
+    private float defaultJumpForce;
 
+    private Rigidbody2D rb;
+
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        defaultJumpForce = GetComponent<PlayerCharacterController>().jumpVelocity;
+    }
 
 
     private void Update() 
     {
-        hit = Physics2D.BoxCast(transform.position, boxCastDimension, 0, Vector2.down, maxCastDistance, terrainMask); 
+        //TODO modificare con trigger collider
+        hit = Physics2D.BoxCast(transform.position + new Vector3(offsetXCast, 0, 0), boxCastDimension, 0, Vector2.down, maxCastDistance, terrainMask); 
 
         if(hit.collider != null)
         {
+
             if(hit.collider.CompareTag("MovingPlatform"))
             {
                 this.transform.SetParent(hit.transform);
+                
+                
+                float platformVelocity = hit.collider.gameObject.GetComponent<UnityEngine.AI.NavMeshAgent>().velocity.x;
+                if(platformVelocity > 0f && rb.velocity.x > 0f
+                    || platformVelocity < 0f && rb.velocity.x < 0f)
+                {
+                    GetComponent<PlayerCharacterController>().jumpVelocity = defaultJumpForce;
+                }
+                else if(platformVelocity == 0f)
+                {
+                    GetComponent<PlayerCharacterController>().jumpVelocity = defaultJumpForce;
+                }
+                else
+                {
+                    GetComponent<PlayerCharacterController>().jumpVelocity = jumpForceBoost;
+                }
+
             }
             else
             {
                 this.transform.SetParent(null);
+                GetComponent<PlayerCharacterController>().jumpVelocity = 700f;
             }
         }
         else
         {
             this.transform.SetParent(null);
+            GetComponent<PlayerCharacterController>().jumpVelocity = 700f;
         }
     }
 
+
+
     private void OnDrawGizmos()
     {
-        Gizmos.DrawWireCube(new Vector3(transform.position.x - offsetXCast,transform.position.y - maxCastDistance, 0), boxCastDimension);
+        Gizmos.DrawWireCube(new Vector3(transform.position.x + offsetXCast, transform.position.y - maxCastDistance, 0), boxCastDimension);
     }
 }
